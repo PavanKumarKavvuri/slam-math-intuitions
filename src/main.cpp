b95b2a498f6e4d3f37454eba988fff374a7273bb
PavanKumarKavvuri <kavvuripavankumar@gmail.com>
2025-07-21 13:18:33 +0100

    The very first lines ofVO development pipeline

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello Pavan, this is our very first step towards VO development pipeline"
+              << std::endl;
+    std::cout << "We will be using C++ and Linear Algebra for this project" << std::endl;
+    std::cout << "Let's get started!" << std::endl;
+    return 0;
+}
